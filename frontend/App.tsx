bc2e0b9359bfe2dde61a20cf38a7d369eadad9f8
mzmbq <100804739+mzmbq@users.noreply.github.com>
2025-08-09 16:04:31 +0200

    Feature/basic UI (#11)
    * Add Nativewind
    
    * Add bottom tabs navigation
    
    * Add Tabler icons

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import "./global.css";
+import SafeAreaWrapper from "./components/SafeAreaWrapper";
+import Routes from "./screens/Routes";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start sadfasdfasfasdfworking on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaWrapper className="flex-1">
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
+    </SafeAreaWrapper>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
