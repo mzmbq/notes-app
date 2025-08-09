3e2baf48b69e49e031fb643a57d5e40b23e8c63e
Dmitriy Riffel <dmitriyriffel1@gmail.com>
2025-08-09 17:58:02 +0200

    Impelemnt updateNote, getNoteById service and controller

--- backend/src/notes/notes.service.ts
+++ backend/src/notes/notes.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from "@nestjs/common";
-import { CreateNoteDto } from "./notes.dto";
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
+import { CreateNoteDto, UpdateNoteDto } from "./notes.dto";
 import { Note } from "src/types/note";
 import { notes } from "src/db/note";
 import { db } from "src/database/db";
+import { eq } from "drizzle-orm";
+import { isUUID } from "class-validator";
 
 @Injectable()
 export class NotesService {
@@ -24,5 +30,38 @@ export class NotesService {
       }
     }
     throw new Error("[createNote] Failed by creating a note (Unknown error)");
+  }
+
+  async getNoteById(id: string): Promise<Note> {
+    if (!isUUID(id)) {
+      throw new BadRequestException(
+        `[getNoteById] Invalid note ID format: "${id}"`,
+      );
+    }
+    const result = await db.select().from(notes).where(eq(notes.id, id));
+    const note = result[0];
+    if (!note) {
+      throw new NotFoundException(
+        `[getNoteById] Note with ID "${id}" not found`,
+      );
+    }
+    return note;
+  }
+
+  async updateNote(id: string, dto: UpdateNoteDto): Promise<Note> {
+    try {
+      await db.update(notes).set(dto).where(eq(notes.id, id));
+    } catch (err) {
+      if (err instanceof Error) {
+        throw new Error(
+          `[updateNote] Failed by updating a note with id: ${id}, `,
+          err,
+        );
+      }
+      throw new Error(
+        `[updateNote] Failed by updating a note with id: ${id} (Unknown Error)`,
+      );
+    }
+    return this.getNoteById(id);
   }
 }
