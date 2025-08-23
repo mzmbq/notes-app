b3b000f1f39a2da79ee5d3f09c7bab4a9f0ce36d
Dmitriy Riffel <dmitriyriffel1@gmail.com>
2025-08-23 18:35:46 +0200

    Rebase db snapshots

--- backend/drizzle/0004_bizarre_captain_flint.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "tag" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"name" varchar NOT NULL,
-	"text_color" varchar NOT NULL,
-	"background_color" varchar NOT NULL
-);
--- backend/drizzle/0005_superb_wallow.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "tag" RENAME COLUMN "name" TO "title";--> statement-breakpoint
-ALTER TABLE "tag" ADD COLUMN "author_id" uuid NOT NULL;--> statement-breakpoint
-ALTER TABLE "tag" ADD CONSTRAINT "tag_author_id_user_id_fk" FOREIGN KEY ("author_id") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;
